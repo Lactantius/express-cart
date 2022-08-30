4678a6f72f94a015e2282377bbe724b31f1d382f
Gerard Keiser <gerardkeiser@gmail.com>
2022-08-30 13:41:14 -0500

    Make delete route; stuck on edit route

--- src/routes.ts
+++ src/routes.ts
@@ -20,6 +20,7 @@ router.post("/", (req, res) => {
     req.body,
     isInstanceOfItem,
     E.chain(addItem),
+    E.chain(makeResponse(201)("added")),
     E.foldW(identity, identity)
   );
   res.statusCode = response.statusCode; /* Mutate state. Why not? */
@@ -30,14 +31,37 @@ router.get("/:name", (req, res) => {
   const response = pipe(
     req.params.name,
     findItem,
-    E.chain(makeResponse(200)),
+    E.chain(makeResponse(200)("item")),
     E.foldW(identity, identity)
   );
   res.statusCode = response.statusCode;
   return res.json(response.body);
 });
 
-// router.patch("/:name", (req, res) => { });
+router.patch("/:name", (req, res) => {
+  const item = findItem(req.params.name);
+  const response = pipe(
+    req.body,
+    isInstanceOfItemEdit,
+    E.chain(editItem(item)) /* What is the best way of handling this? */,
+    E.chain(makeResponse(200)("edited")),
+    E.foldW(identity, identity)
+  );
+  res.statusCode = response.statusCode;
+  return res.json(response.body);
+});
+
+router.delete("/:name", (req, res) => {
+  const response = pipe(
+    req.params.name,
+    findItem,
+    E.chain(deleteItem),
+    E.chain(makeResponse(200)("deleted")),
+    E.foldW(identity, identity)
+  );
+  res.statusCode = response.statusCode;
+  return res.json(response.body);
+});
 
 /*
  * Helpers
@@ -45,9 +69,11 @@ router.get("/:name", (req, res) => {
 
 const makeResponse: (
   code: number
-) => (item: Item) => E.Either<ErrorResponse, SuccessResponse> =
-  (code) => (item) =>
-    E.right({ body: { item: item }, statusCode: code });
+) => (
+    msg: string
+  ) => (item: Item) => E.Either<ErrorResponse, SuccessResponse> =
+  (code) => (msg) => (item) =>
+    E.right({ body: { [msg]: item }, statusCode: code });
 
 function findItem(name: string): E.Either<ErrorResponse, Item> {
   const item = items.find(
@@ -58,21 +84,29 @@ function findItem(name: string): E.Either<ErrorResponse, Item> {
     : E.left(makeError(`Item '${name}' not found`, 404));
 }
 
-function addItem(item: Item): E.Either<ErrorResponse, SuccessResponse> {
+const editItem: (
+  item: Item
+) => (edits: ItemEdit) => E.Either<ErrorResponse, Item> = (item) => (edits) =>
+  E.right(Object.assign(item, edits));
+
+function addItem(item: Item): E.Either<ErrorResponse, Item> {
   return pipe(
     E.tryCatch(
       () => items.push(item),
       (err) => makeError(`Item ${item} could not be added`, 500)
     ),
-    E.map(
-      () =>
-      ({
-        body: {
-          added: item,
-        },
-        statusCode: 201,
-      } as SuccessResponse)
-    )
+    E.map(() => item)
+  );
+}
+
+function deleteItem(item: Item): E.Either<ErrorResponse, Item> {
+  const index = items.indexOf(item);
+  return pipe(
+    E.tryCatch(
+      () => items.splice(index, 1),
+      (err) => makeError(`Item ${item} could be be deleted`, 500)
+    ),
+    E.map(() => item)
   );
 }
 
@@ -87,6 +121,14 @@ function isInstanceOfItem(obj: object): E.Either<ErrorResponse, Item> {
 
 function makeError(msg: string, code: number): ErrorResponse {
   return { body: { error: msg }, statusCode: code };
+}
+
+function isInstanceOfItemEdit(obj: object): E.Either<ErrorResponse, ItemEdit> {
+  const props = ["name", "price"];
+  const isEdit = typeof obj === "object" && props.some((prop) => prop in obj);
+  return isEdit
+    ? E.right(obj as Item)
+    : E.left(makeError("Not a valid edit", 400));
 }
 
 /*
@@ -105,6 +147,11 @@ interface SuccessResponse {
     [index: string]: Item;
   };
   statusCode: number;
+}
+
+interface ItemEdit {
+  name?: string;
+  price?: number;
 }
 
 export { router };
